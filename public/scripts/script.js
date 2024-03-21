961f124b1c1718c19960d19f7d6d8b2accfd75fa
Robert Thao <robert.lee.thao@gmail.com>
2024-03-21 04:39:20 -0500

    submitting now gives you a console log of totals for duration, sets, and reps for the current week

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -10,6 +10,8 @@ let currentWeekLog = [
     // }
 ];
 
+
+//Stretch Goal: Optionally, include a structure to track daily or weekly goals and accomplishments.
 let lastWeekLog = [];
 
 let lifeTimeStats = [];
@@ -28,7 +30,7 @@ function displayInfo(array, elementId) {
 
 function logWorkout() {
     let name = document.getElementById("workout-type-input").value;
-    let duration = document.getElementById("workout-duration-input").value;
+    let duration = Number(document.getElementById("workout-duration-input").value);
     let difficulty;
 
     function findSelectedValue() {
@@ -42,8 +44,8 @@ function logWorkout() {
     };
     findSelectedValue(difficulty);
 
-    let sets = document.getElementById("workout-sets-input").value;
-    let reps = document.getElementById("workout-reps-input").value;
+    let sets = Number(document.getElementById("workout-sets-input").value);
+    let reps = Number(document.getElementById("workout-reps-input").value);
     
     // console.log(name, duration, difficulty, sets, reps);
 
@@ -60,14 +62,27 @@ function logWorkout() {
     displayInfo(currentWeekLog, 'current-log')
 }
 
-
+// future function for user to add additional workouts to available selection
 function addNewWorkout(){
     console.log("test");
+}
+
+// **Workout Summary Calculation:**
+// Create at least two functions to calculate summaries from the exercise log data, such as total workouts completed, 
+//total duration, or calories burned (if applicable). Keep in mind these numbers will later need to be displayed.
+
+function calculateTotals(array){
+    console.log(
+        array.reduce((total, time) => total+time.duration, 0),
+        array.reduce((total, time) => total+time.sets, 0),
+        array.reduce((total, time) => total+time.reps, 0),
+    );
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     
     document.getElementById('log-this-workout').onclick = () => {
         logWorkout();
+        calculateTotals(currentWeekLog);
     };
 });
